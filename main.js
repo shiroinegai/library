2a6c892a990e44b02f9d9cc4f3db094d4ca0a5ef
Shiroi Negai <shiroinegai@pm.me>
2021-08-22 04:59:58 +0800

    Implement basic library functions and CSS
    Library functions as follows:
    Book constructor,
    addBookToLibrary,
    displayBooks,
    createBookNode,
    appendToLibraryNode

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 console.log("Library is running.");
+
+const library = [
+  { title: "Deatte 5-byou de Battle" },
+  { title: "Tensei Shitara Slime Datta Ken" },
+  { title: "Kobayashi-san Chi no Maid Dragon" },
+];
+
+const libraryNode = document.getElementById("library");
+
+function Book(title) {
+  this.title = title;
+}
+
+function addBookToLibrary(book) {
+  library.push(book);
+}
+
+function displayBooks() {
+  library.forEach((book) => {
+    appendToLibraryNode(createBookNode(book));
+  });
+}
+
+function createBookNode(book) {
+  const bookNode = document.createElement("li");
+
+  bookNode.append(`${book.title}`);
+
+  return bookNode;
+}
+
+function appendToLibraryNode(bookNode) {
+  libraryNode.append(bookNode);
+}
+
+displayBooks();
